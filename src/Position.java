29f000f6af7c6ea65e1eb784723a8e273d14b18c
Najim <2020-04-01522>
2022-05-14 11:10:30 +0300

    lab 1 assignment

---
+++ src/Position.java
@@ -0,0 +1,19 @@
+public class Position {
+
+    public static void main(String[] args){
+        double g=-9.81;
+        double t=10;
+        double vi=0.0;
+        double xi=0.0;
+        double x;
+        x=(0.5*((g*t*t))+(vi*t)+(xi));
+        System.out.println( x +"m");
+
+
+
+    }
+
+
+
+
+}
